93eb439e4501902f793a64835dc7c52592901eb9
Hannes Achleitner <hannes.software@gmx.at>
2022-12-19 08:43:32 +0100

    Fix permission test

--- examples/src/androidTest/java/org/rajawali3d/examples/LauncherSmokeTest.kt
+++ examples/src/androidTest/java/org/rajawali3d/examples/LauncherSmokeTest.kt
@@ -1,5 +1,6 @@
 package org.rajawali3d.examples
 
+import android.Manifest
 import androidx.test.core.graphics.writeToTestStorage
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -9,6 +10,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.screenshot.captureToBitmap
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestName
@@ -22,6 +24,13 @@ class LauncherSmokeTest {
 
     @get:Rule
     var nameRule = TestName()
+
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
 
     @Test
     fun smokeTestSimplyStart() {
