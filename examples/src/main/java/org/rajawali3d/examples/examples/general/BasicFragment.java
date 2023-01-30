ef956c320703dc748d691c96134241c4e5f0457b
Hannes Achleitner <hannes.software@gmx.at>
2023-01-30 09:31:23 +0100

    Code format

--- examples/src/main/java/org/rajawali3d/examples/examples/general/BasicFragment.java
+++ examples/src/main/java/org/rajawali3d/examples/examples/general/BasicFragment.java
@@ -1,7 +1,9 @@
 package org.rajawali3d.examples.examples.general;
 
 import android.content.Context;
+
 import androidx.annotation.Nullable;
+
 import org.rajawali3d.Object3D;
 import org.rajawali3d.examples.R;
 import org.rajawali3d.examples.examples.AExampleFragment;
@@ -15,44 +17,44 @@ import timber.log.Timber;
 
 public class BasicFragment extends AExampleFragment {
 
-	@Override
+    @Override
     public AExampleRenderer createRenderer() {
-		return new BasicRenderer(getActivity(), this);
-	}
+        return new BasicRenderer(getActivity(), this);
+    }
 
-	public static final class BasicRenderer extends AExampleRenderer {
+    public static final class BasicRenderer extends AExampleRenderer {
 
-		private Object3D mSphere;
+        private Object3D mSphere;
 
-		public BasicRenderer(Context context, @Nullable AExampleFragment fragment) {
-			super(context, fragment);
-		}
+        public BasicRenderer(Context context, @Nullable AExampleFragment fragment) {
+            super(context, fragment);
+        }
 
         @Override
-		protected void initScene() {
-			try {
-				Material material = new Material();
-				material.addTexture(new Texture("earthColors",
-												R.drawable.earthtruecolor_nasa_big));
-				material.setColorInfluence(0);
-				mSphere = new Sphere(1, 24, 24);
-				mSphere.setMaterial(material);
-				getCurrentScene().addChild(mSphere);
-			} catch (ATexture.TextureException e) {
-				Timber.e(e);
-			}
+        protected void initScene() {
+            try {
+                Material material = new Material();
+                material.addTexture(new Texture("earthColors",
+                        R.drawable.earthtruecolor_nasa_big));
+                material.setColorInfluence(0);
+                mSphere = new Sphere(1, 24, 24);
+                mSphere.setMaterial(material);
+                getCurrentScene().addChild(mSphere);
+            } catch (ATexture.TextureException e) {
+                Timber.e(e);
+            }
 
-			Timber.d("Camera initial orientation: %s", getCurrentCamera().getOrientation());
+            Timber.d("Camera initial orientation: %s", getCurrentCamera().getOrientation());
             getCurrentCamera().enableLookAt();
             getCurrentCamera().setLookAt(0, 0, 0);
             getCurrentCamera().setZ(6);
-			getCurrentCamera().setOrientation(getCurrentCamera().getOrientation().inverse());
+            getCurrentCamera().setOrientation(getCurrentCamera().getOrientation().inverse());
         }
 
         @Override
         public void onRender(final long elapsedTime, final double deltaTime) {
-			super.onRender(elapsedTime, deltaTime);
-			mSphere.rotate(Vector3.Axis.Y, 1.0);
-		}
-	}
+            super.onRender(elapsedTime, deltaTime);
+            mSphere.rotate(Vector3.Axis.Y, 1.0);
+        }
+    }
 }
